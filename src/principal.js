06888d7f04cf8c1e7d5bda700887f242fe6a7331
Henrique França <henriqsf@gmail.com>
2022-12-08 12:48:39 -0300

    Sistema de Módulos

--- src/principal.js
+++ src/principal.js
@@ -1,4 +1,6 @@
 import Pessoa from "./pessoa";
+//so tem como interagir com a pasta modulos se estiver explicitamente referenciado
+import './modulos/moduloA'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
