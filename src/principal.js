119ad73b210fcbe1c4de608763a308c58c8a39c6
Henrique França <henriqsf@gmail.com>
2022-12-09 07:22:39 -0300

    Carregando Arquivo .CSS

--- src/principal.js
+++ src/principal.js
@@ -1,6 +1,8 @@
 import Pessoa from "./pessoa";
+import './assets/css/estilo.css'
 //so tem como interagir com a pasta modulos se estiver explicitamente referenciado
-import './modulos/moduloA'
+//import './modulos/moduloA'
+
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
