352c493a2701f82dbe9037069d55d96775d7e2d3
Henrique França <henriqsf@gmail.com>
2022-12-09 08:29:33 -0300

    feat: Carregando Arquivos de Imagens

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,9 @@ module.exports = {
                 'css-loader', // interpretar @import, url...
                 'sass-loader',
             ]
+        },{
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
